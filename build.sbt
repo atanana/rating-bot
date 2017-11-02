f4fe40a6e58d66fff0076c5bda4c5d8c97e56e4d
atanana <tanana@email.ru>
2017-11-02 20:56:39 +0300

    new teams page parsing

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "rating-bot"
 
-version := "1.10.0"
+version := "1.10.1"
 
 scalaVersion := "2.12.2"
 
--- src/main/scala/com/atanana/providers/TeamPositionsInfoComposer.scala
+++ src/main/scala/com/atanana/providers/TeamPositionsInfoComposer.scala
@@ -7,7 +7,7 @@ import scala.util.Try
 class TeamPositionsInfoComposer(teamId: Int) {
   def positionsInfo(teams: List[Team], cityTeams: List[Team], countryTeams: List[Team]): Try[TeamPositionsInfo] = {
     Try {
-      val lastTeam = teams(99)
+      val lastTeam = getLastTop100Team(teams)
       val team = teams.find(_.id == teamId)
         .getOrElse(throw new RuntimeException("No target team in top 500!"))
       val targetTeam = teams(teams.indexOf(team) - 1)
@@ -21,6 +21,10 @@ class TeamPositionsInfoComposer(teamId: Int) {
         getPosition(countryTeams, "country")
       )
     }
+  }
+
+  private def getLastTop100Team(teams: List[Team]) = {
+    teams.find(_.position > 99.9).get
   }
 
   private def getPosition(teams: List[Team], listName: String) = {
