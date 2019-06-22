5f436f6886f39933e4b093f299135cab1eed89cb
Andrei Tanana <tanana@email.ru>
2019-06-22 12:53:53 +0300

    refactoring

--- src/main/scala/com/atanana/processors/PollProcessor.scala
+++ src/main/scala/com/atanana/processors/PollProcessor.scala
@@ -1,12 +1,11 @@
 package com.atanana.processors
 
-import javax.inject.Inject
-
 import com.atanana.CheckResultHandler
 import com.atanana.checkers.MainChecker
 import com.atanana.data.{Data, ParsedData}
 import com.atanana.json.JsonStore
 import com.atanana.providers.PollingDataProvider
+import javax.inject.Inject
 
 class PollProcessor @Inject()(pollingDataProvider: PollingDataProvider, store: JsonStore, checker: MainChecker,
                               checkResultHandler: CheckResultHandler) extends Processor {
@@ -22,10 +21,15 @@ class PollProcessor @Inject()(pollingDataProvider: PollingDataProvider, store: J
     }
   }
 
-  private def hasChanges(storedData: Data, parsedData: ParsedData) = {
-    storedData.tournaments != parsedData.tournaments.map(_.toTournament) || parsedData.requisitions.fold(
+  private def hasChanges(storedData: Data, parsedData: ParsedData) =
+    tournamentsChanged(storedData, parsedData) || requisitionsChanged(storedData, parsedData)
+
+  private def requisitionsChanged(storedData: Data, parsedData: ParsedData) =
+    parsedData.requisitions.fold(
       _ => false,
       requisitions => storedData.requisitions != requisitions.map(_.toRequisition)
     )
-  }
+
+  private def tournamentsChanged(storedData: Data, parsedData: ParsedData) =
+    storedData.tournaments != parsedData.tournaments.map(_.toTournament)
 }
