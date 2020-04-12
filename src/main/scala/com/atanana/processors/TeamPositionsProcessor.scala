6d1a7729d246d977f7c862911b0201129d49a356
Andrei Tanana <tanana@email.ru>
2020-04-12 15:29:27 +0300

    add logging

--- src/main/scala/com/atanana/processors/TeamPositionsProcessor.scala
+++ src/main/scala/com/atanana/processors/TeamPositionsProcessor.scala
@@ -1,17 +1,30 @@
 package com.atanana.processors
 
 import javax.inject.Inject
-
 import com.atanana.MessageComposer
 import com.atanana.posters.Poster
 import com.atanana.providers.TeamPositionsInfoProvider
+import com.typesafe.scalalogging.Logger
 
-class TeamPositionsProcessor @Inject()(infoProvider: TeamPositionsInfoProvider,
-                                       messageComposer: MessageComposer,
-                                       poster: Poster) extends Processor {
+class TeamPositionsProcessor @Inject()(
+                                        infoProvider: TeamPositionsInfoProvider,
+                                        messageComposer: MessageComposer,
+                                        poster: Poster
+                                      ) extends Processor {
+
+  import TeamPositionsProcessor.logger
+
   override def process(): Unit = {
-    infoProvider.data
+    val result = infoProvider.data
       .map(messageComposer.composeTeamPositionsMessage)
       .map(poster.post)
+    result match {
+      case Left(errorMessage) => logger.error(errorMessage)
+      case Right(_) => // do nothing
+    }
   }
+}
+
+object TeamPositionsProcessor {
+  private val logger = Logger(classOf[TeamPositionsProcessor])
 }
