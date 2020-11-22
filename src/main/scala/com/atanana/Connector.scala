fa09a472f02d1cdc4645b9379768dd73694bfd5d
Andrei Tanana <tanana@email.ru>
2020-11-22 15:13:54 +0300

    Connector refactoring

--- src/main/scala/com/atanana/Connector.scala
+++ src/main/scala/com/atanana/Connector.scala
@@ -12,46 +12,44 @@ class Connector @Inject()(netWrapper: NetWrapper, config: Config) {
 
   private def getPage(uri: Uri) = netWrapper.getPage(uri)
 
-  private def getPageSafe(uri: Uri) = netWrapper.getPageSafe(uri)
-
   def getTeamPage: Either[String, String] = {
     val url = uri"$SITE_URL/teams.php?team_id=${config.team}&download_data=export_tournaments"
-    getPageSafe(url).left.map(error => s"Cannot get team's page($url): $error")
+    getPage(url).left.map(error => s"Cannot get team's page($url): $error")
   }
 
   def getTournamentPage(id: Int): Either[String, String] = {
     val url = uri"$SITE_URL/tournament/$id"
-    getPageSafe(url).left.map(error => s"Cannot get tournament's page($url): $error")
+    getPage(url).left.map(error => s"Cannot get tournament's page($url): $error")
   }
 
   def getRequisitionPage: Either[String, String] = {
     val url = uri"$SITE_URL/synch_town/${config.city}"
-    getPageSafe(url).left.map(error => s"Cannot get requisitions page($url): $error")
+    getPage(url).left.map(error => s"Cannot get requisitions page($url): $error")
   }
 
   def getTeamsPage: Either[String, String] = {
     val url = uri"$SITE_URL/teams.php"
-    getPageSafe(url).left.map(error => s"Cannot get teams page($url): $error")
+    getPage(url).left.map(error => s"Cannot get teams page($url): $error")
   }
 
   def getCityTeamsPage: Either[String, String] = {
     val url = uri"$SITE_URL/teams.php?town=${config.cityName}"
-    getPageSafe(url).left.map(error => s"Cannot get city teams page($url): $error")
+    getPage(url).left.map(error => s"Cannot get city teams page($url): $error")
   }
 
   def getCountryTeamsPage: Either[String, String] = {
     val url = uri"$SITE_URL/teams.php?country=${config.countryName}"
-    getPageSafe(url).left.map(error => s"Cannot get country teams page($url): $error")
+    getPage(url).left.map(error => s"Cannot get country teams page($url): $error")
   }
 
   def getTournamentRequisitionsPage(tournamentId: Int): Either[String, String] = {
     val url = uri"$SITE_URL/tournament/$tournamentId/requests"
-    getPageSafe(url).left.map(error => s"Cannot get tournament requisitions page($url): $error")
+    getPage(url).left.map(error => s"Cannot get tournament requisitions page($url): $error")
   }
 
   def getTournamentInfo(tournamentId: Int): Either[String, String] = {
     val url = uri"$SITE_URL/api/tournaments/$tournamentId.json"
-    getPageSafe(url).left.map(error => s"Cannot get tournament info page($url): $error")
+    getPage(url).left.map(error => s"Cannot get tournament info page($url): $error")
   }
 }
 
@@ -63,13 +61,11 @@ object Connector {
 class NetWrapper {
   private val backend = HttpURLConnectionBackend()
 
-  def getPageSafe(uri: Uri): Either[String, String] =
+  def getPage(uri: Uri): Either[String, String] =
     basicRequest
       .get(uri)
       .send(backend)
       .body
-
-  def getPage(uri: Uri): String = getPageSafe(uri).right.get
 
   def post(uri: Uri, params: Map[String, String]): Either[String, String] =
     basicRequest
--- src/test/scala/com/atanana/ConnectorTest.scala
+++ src/test/scala/com/atanana/ConnectorTest.scala
@@ -17,96 +17,96 @@ class ConnectorTest extends AnyWordSpecLike with MockFactory with Matchers {
 
     "get team page by wrapper" in {
       val teamUrl = uri"$SITE_URL/teams.php?team_id=${config.team}&download_data=export_tournaments"
-      (wrapper.getPageSafe _).when(teamUrl).returns(Right("team page"))
+      (wrapper.getPage _).when(teamUrl).returns(Right("team page"))
       connector.getTeamPage shouldEqual Right("team page")
     }
 
     "pass team page error from wrapper" in {
       val teamUrl = uri"$SITE_URL/teams.php?team_id=${config.team}&download_data=export_tournaments"
-      (wrapper.getPageSafe _).when(teamUrl).returns(Left("123"))
+      (wrapper.getPage _).when(teamUrl).returns(Left("123"))
       connector.getTeamPage shouldEqual Left(s"Cannot get team's page($teamUrl): 123")
     }
 
     "get tournament page by wrapper" in {
       val tournamentId = 111
-      (wrapper.getPageSafe _).when(uri"$SITE_URL/tournament/$tournamentId").returns(Right("tournament page"))
+      (wrapper.getPage _).when(uri"$SITE_URL/tournament/$tournamentId").returns(Right("tournament page"))
       connector.getTournamentPage(tournamentId) shouldEqual Right("tournament page")
     }
 
     "pass tournament page error from wrapper" in {
       val tournamentId = 111
       val tournamentUrl = uri"$SITE_URL/tournament/$tournamentId"
-      (wrapper.getPageSafe _).when(tournamentUrl).returns(Left("123"))
+      (wrapper.getPage _).when(tournamentUrl).returns(Left("123"))
       connector.getTournamentPage(tournamentId) shouldEqual Left(s"Cannot get tournament's page($tournamentUrl): 123")
     }
 
     "get requisitions page by wrapper" in {
-      (wrapper.getPageSafe _).when(uri"$SITE_URL/synch_town/${config.city}").returns(Right("requisitions page"))
+      (wrapper.getPage _).when(uri"$SITE_URL/synch_town/${config.city}").returns(Right("requisitions page"))
       connector.getRequisitionPage shouldEqual Right("requisitions page")
     }
 
     "pass requisitions page error from wrapper" in {
       val requisitionsUrl = uri"$SITE_URL/synch_town/${config.city}"
-      (wrapper.getPageSafe _).when(requisitionsUrl).returns(Left("123"))
+      (wrapper.getPage _).when(requisitionsUrl).returns(Left("123"))
       connector.getRequisitionPage shouldEqual Left(s"Cannot get requisitions page($requisitionsUrl): 123")
     }
 
     "get teams page by wrapper" in {
-      (wrapper.getPageSafe _).when(uri"$SITE_URL/teams.php").returns(Right("teams page"))
+      (wrapper.getPage _).when(uri"$SITE_URL/teams.php").returns(Right("teams page"))
       connector.getTeamsPage shouldEqual Right("teams page")
     }
 
     "pass teams page error from wrapper" in {
       val teamsUrl = uri"$SITE_URL/teams.php"
-      (wrapper.getPageSafe _).when(teamsUrl).returns(Left("123"))
+      (wrapper.getPage _).when(teamsUrl).returns(Left("123"))
       connector.getTeamsPage shouldEqual Left(s"Cannot get teams page($teamsUrl): 123")
     }
 
     "get city teams page by wrapper" in {
-      (wrapper.getPageSafe _).when(uri"$SITE_URL/teams.php?town=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA").returns(Right("city teams page"))
+      (wrapper.getPage _).when(uri"$SITE_URL/teams.php?town=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA").returns(Right("city teams page"))
       connector.getCityTeamsPage shouldEqual Right("city teams page")
     }
 
     "pass city teams page error from wrapper" in {
       val teamsUrl = uri"$SITE_URL/teams.php?town=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA"
-      (wrapper.getPageSafe _).when(teamsUrl).returns(Left("123"))
+      (wrapper.getPage _).when(teamsUrl).returns(Left("123"))
       connector.getCityTeamsPage shouldEqual Left(s"Cannot get city teams page($teamsUrl): 123")
     }
 
     "get country teams page by wrapper" in {
-      (wrapper.getPageSafe _).when(uri"$SITE_URL/teams.php?country=%D0%91%D0%B5%D0%BB%D0%B0%D1%80%D1%83%D1%81%D1%8C").returns(Right("country teams page"))
+      (wrapper.getPage _).when(uri"$SITE_URL/teams.php?country=%D0%91%D0%B5%D0%BB%D0%B0%D1%80%D1%83%D1%81%D1%8C").returns(Right("country teams page"))
       connector.getCountryTeamsPage shouldEqual Right("country teams page")
     }
 
     "pass country teams page error from wrapper" in {
       val teamsUrl = uri"$SITE_URL/teams.php?country=%D0%91%D0%B5%D0%BB%D0%B0%D1%80%D1%83%D1%81%D1%8C"
-      (wrapper.getPageSafe _).when(teamsUrl).returns(Left("123"))
+      (wrapper.getPage _).when(teamsUrl).returns(Left("123"))
       connector.getCountryTeamsPage shouldEqual Left(s"Cannot get country teams page($teamsUrl): 123")
     }
 
     "get tournament requisitions page by wrapper" in {
       val tournamentId = 111
-      (wrapper.getPageSafe _).when(uri"$SITE_URL/tournament/$tournamentId/requests").returns(Right("tournament requests page"))
+      (wrapper.getPage _).when(uri"$SITE_URL/tournament/$tournamentId/requests").returns(Right("tournament requests page"))
       connector.getTournamentRequisitionsPage(tournamentId) shouldEqual Right("tournament requests page")
     }
 
     "pass tournament requisitions page error from wrapper" in {
       val tournamentId = 111
       val url = uri"$SITE_URL/tournament/$tournamentId/requests"
-      (wrapper.getPageSafe _).when(url).returns(Left("123"))
+      (wrapper.getPage _).when(url).returns(Left("123"))
       connector.getTournamentRequisitionsPage(tournamentId) shouldEqual Left(s"Cannot get tournament requisitions page($url): 123")
     }
 
     "get tournament info page by wrapper" in {
       val tournamentId = 111
-      (wrapper.getPageSafe _).when(uri"$SITE_URL/api/tournaments/$tournamentId.json").returns(Right("tournament info page"))
+      (wrapper.getPage _).when(uri"$SITE_URL/api/tournaments/$tournamentId.json").returns(Right("tournament info page"))
       connector.getTournamentInfo(tournamentId) shouldEqual Right("tournament info page")
     }
 
     "pass tournament info page error from wrapper" in {
       val tournamentId = 111
       val url = uri"$SITE_URL/api/tournaments/$tournamentId.json"
-      (wrapper.getPageSafe _).when(url).returns(Left("123"))
+      (wrapper.getPage _).when(url).returns(Left("123"))
       connector.getTournamentInfo(tournamentId) shouldEqual Left(s"Cannot get tournament info page($url): 123")
     }
   }
