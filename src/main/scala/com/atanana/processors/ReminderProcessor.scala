b293d326f7ba333f6e88463c3146da8db40f5453
Andrei Tanana <tanana@email.ru>
2020-12-03 21:27:03 +0300

    ReminderProcessor refactoring

--- src/main/scala/com/atanana/processors/ReminderProcessor.scala
+++ src/main/scala/com/atanana/processors/ReminderProcessor.scala
@@ -3,6 +3,7 @@ package com.atanana.processors
 import com.atanana.MessageComposer
 import com.atanana.json.JsonStore
 import com.atanana.posters.Poster
+import com.atanana.utils.CollectionsUtils.eitherSet
 
 import java.time.LocalDate
 import javax.inject.Inject
@@ -11,10 +12,9 @@ class ReminderProcessor @Inject()(store: JsonStore, messageComposer: MessageComp
   override def process(): Either[String, Unit] = {
     val data = store.read
     val tomorrow = LocalDate.now().plusDays(1)
-    data.requisitions
+    val messages = data.requisitions
       .filter(requisition => requisition.dateTime.toLocalDate == tomorrow)
       .map(messageComposer.composeRequisitionReminder)
-      .foreach(poster.post)
-    Right()
+    eitherSet(messages.map(poster.post)).map(_ => Unit)
   }
 }
--- src/test/scala/com/atanana/processors/ReminderProcessorTest.scala
+++ src/test/scala/com/atanana/processors/ReminderProcessorTest.scala
@@ -1,33 +1,26 @@
 package com.atanana.processors
 
-import java.time.LocalDateTime
-
 import com.atanana.MessageComposer
 import com.atanana.data.{Data, Requisition}
 import com.atanana.json.JsonStore
 import com.atanana.posters.Poster
 import org.scalamock.scalatest.MockFactory
-import org.scalatest.{BeforeAndAfter, WordSpecLike}
+import org.scalatest.wordspec.AnyWordSpecLike
 
-class ReminderProcessorTest extends WordSpecLike with MockFactory with BeforeAndAfter {
-  var store: JsonStore = _
-  var messageComposer: MessageComposer = _
-  var poster: Poster = _
-  var processor: ReminderProcessor = _
+import java.time.LocalDateTime
 
-  before {
-    store = stub[JsonStore]
-    messageComposer = stub[MessageComposer]
-    poster = mock[Poster]
-    processor = new ReminderProcessor(store, messageComposer, poster)
-  }
+class ReminderProcessorTest extends AnyWordSpecLike with MockFactory {
+  private val store = stub[JsonStore]
+  private val messageComposer = stub[MessageComposer]
+  private val poster = mock[Poster]
+  private val processor = new ReminderProcessor(store, messageComposer, poster)
 
   "ReminderProcessor" should {
     "post correct reminders for tomorrow's requisitions" in {
       val requisition = Requisition("tournament 1", "agent 1", LocalDateTime.now().plusDays(1))
       (store.read _).when().returns(Data(Set.empty, Set(requisition)))
       (messageComposer.composeRequisitionReminder _).when(requisition).returns("reminder")
-      (poster.post _).expects("reminder")
+      (poster.post _).expects("reminder") returns Right()
 
       processor.process()
     }
