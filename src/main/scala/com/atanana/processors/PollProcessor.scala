82d7d0c7c149c2c2677e422c2e49d7976be6c84f
Andrei Tanana <tanana@email.ru>
2020-12-03 21:59:09 +0300

    PollProcessor refactoring

--- src/main/scala/com/atanana/processors/PollProcessor.scala
+++ src/main/scala/com/atanana/processors/PollProcessor.scala
@@ -4,9 +4,7 @@ import com.atanana.CheckResultHandler
 import com.atanana.checkers.MainChecker
 import com.atanana.data.{Data, ParsedData}
 import com.atanana.json.JsonStore
-import com.atanana.processors.PollProcessor.logger
 import com.atanana.providers.PollingDataProvider
-import com.typesafe.scalalogging.Logger
 
 import javax.inject.Inject
 
@@ -18,22 +16,16 @@ class PollProcessor @Inject()(
                              ) extends Processor {
 
   override def process(): Either[String, Unit] = {
-    val parsedData = pollingDataProvider.data.fold(
-      error => {
-        logger.error(error)
-        return Right()
-      },
-      identity
-    )
-    val storedData = store.read
-
-    val checkResult = checker.check(storedData, parsedData)
-    val isPostSuccessful = checkResultHandler.processCheckResult(checkResult).isRight
-
-    if (isPostSuccessful && hasChanges(storedData, parsedData)) {
-      store.write(parsedData.toData)
+    for {
+      parsedData <- pollingDataProvider.data
+      storedData = store.read
+      checkResult = checker.check(storedData, parsedData)
+      _ <- checkResultHandler.processCheckResult(checkResult)
+    } yield {
+      if (hasChanges(storedData, parsedData)) {
+        store.write(parsedData.toData)
+      }
     }
-    Right()
   }
 
   private def hasChanges(storedData: Data, parsedData: ParsedData) =
@@ -47,8 +39,4 @@ class PollProcessor @Inject()(
 
   private def tournamentsChanged(storedData: Data, parsedData: ParsedData) =
     storedData.tournaments != parsedData.tournaments.map(_.toTournament)
-}
-
-object PollProcessor {
-  private val logger = Logger(classOf[PollProcessor])
 }
