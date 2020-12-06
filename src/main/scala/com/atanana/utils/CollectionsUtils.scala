5bf592cdc1cb1bffc120f2ae0e26c9ca5e538673
Andrei Tanana <tanana@email.ru>
2020-12-06 12:00:27 +0300

    refactoring

--- src/main/scala/com/atanana/utils/CollectionsUtils.scala
+++ src/main/scala/com/atanana/utils/CollectionsUtils.scala
@@ -4,6 +4,6 @@ object CollectionsUtils {
 
   def eitherSet[A, B](s: Set[Either[A, B]]): Either[A, Set[B]] =
     s.foldRight(Right(Set.empty): Either[A, Set[B]]) {
-      (e, acc) => for (xs <- acc.right; x <- e.right) yield xs + x
+      (e, acc) => for (xs <- acc; x <- e) yield xs + x
     }
 }
