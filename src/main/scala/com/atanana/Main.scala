c41a7ba142073e9196732572f7aa20965df89403
Andrei Tanana <tanana@email.ru>
2020-12-16 19:37:50 +0300

    log connector exceptions

--- src/main/scala/com/atanana/Main.scala
+++ src/main/scala/com/atanana/Main.scala
@@ -41,6 +41,7 @@ object Main {
       } catch {
         case e: SocketTimeoutException => logger.info("Timeout!", e)
         case e: ConnectException => logger.info("Connect error!", e)
+        case e: ConnectorException => logger.info(s"Error by url ${e.uri}", e)
         case e: Throwable => logger.debug("Error occurred!", e)
       }
     }
