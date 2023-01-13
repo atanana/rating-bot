d930ce6503377427a41a3be792a9874bce94b05b
Andrei Tanana <tanana.andrey@gmail.com>
2023-01-13 17:55:29 +0300

    don't crash when cannot parse team

--- src/main/scala/com/atanana/parsers/TeamsPageParser.scala
+++ src/main/scala/com/atanana/parsers/TeamsPageParser.scala
@@ -3,14 +3,16 @@ package com.atanana.parsers
 import com.atanana.data.Team
 import spray.json._
 
+import scala.util.Try
+
 class TeamsPageParser {
   def getTeams(teamsContent: String): List[Team] = {
     val json = teamsContent.parseJson.asJsObject
     json.fields("data").asInstanceOf[JsArray].elements
-      .map(parseTeam).toList
+      .flatMap(parseTeam(_).toList).toList
   }
 
-  private def parseTeam(json: JsValue): Team = {
+  private def parseTeam(json: JsValue) = Try {
     val teamJson = json.asJsObject
     Team(
       id = teamJson.fields("id").toString().toInt,
@@ -18,5 +20,5 @@ class TeamsPageParser {
       city = teamJson.fields("townName").asInstanceOf[JsString].value,
       rating = teamJson.fields("teamRating").toString().toInt,
       position = teamJson.fields("teamRatingPosition").toString().toInt)
-  }
+  }.toOption
 }
