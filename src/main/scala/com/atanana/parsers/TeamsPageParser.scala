95b61f81f38da53b74d09bf4d0a286a5dfe39635
Andrei Tanana <tanana.andrey@gmail.com>
2023-01-13 16:14:16 +0300

    change teams parser to json

--- src/main/scala/com/atanana/parsers/TeamsPageParser.scala
+++ src/main/scala/com/atanana/parsers/TeamsPageParser.scala
@@ -1,45 +1,24 @@
 package com.atanana.parsers
 
 import com.atanana.data.Team
-import net.ruippeixotog.scalascraper.browser.JsoupBrowser
-import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
-import net.ruippeixotog.scalascraper.dsl.DSL._
-import net.ruippeixotog.scalascraper.model.Element
-
-import scala.util.Try
+import spray.json._
 
 class TeamsPageParser {
-  def getTeams(html: String): List[Team] = {
-    val document = JsoupBrowser().parseString(html)
-    val teamRows = document >> elementList("table.teams_table tbody tr")
-    teamRows.map(tryParseTeam).flatMap(_.toOption.toList)
+  def getTeams(teamsContent: String): List[Team] = {
+    val json = teamsContent.parseJson.asJsObject
+    json.fields("data").asInstanceOf[JsArray].elements
+      .map(parseTeam).toList
   }
 
-  private def tryParseTeam(row: Element) = {
-    Try {
-      val data = row.children.toList
-      val isRealTeam = isReal(data(7))
-      Team(
-        id = parseId(data(6)),
-        name = parseName(data(7), isRealTeam),
-        city = data(8).text,
-        rating = data(3).text.toInt,
-        position = data(1).text.replace(',', '.').toFloat,
-        isReal = isRealTeam
-      )
-    }
+  private def parseTeam(json: JsValue): Team = {
+    val teamJson = json.asJsObject
+    Team(
+      id = teamJson.fields("id").toString().toInt,
+      name = teamJson.fields("teamName").asInstanceOf[JsString].value,
+      city = teamJson.fields("townName").asInstanceOf[JsString].value,
+      rating = teamJson.fields("teamRating").toString().toInt,
+      position = teamJson.fields("teamRatingPosition").toString().toInt,
+      isReal = true
+    )
   }
-
-  private def parseId(idElement: Element): Int = idElement.text.filter(_.toInt != 160).toInt
-
-  private def parseName(nameElement: Element, isReal: Boolean): String = {
-    val name = nameElement.text
-    if (!isReal && name.endsWith(" #")) {
-      name.dropRight(2)
-    } else {
-      name
-    }
-  }
-
-  private def isReal(nameElement: Element): Boolean = (nameElement >?> element(".non_dbs_team")).isEmpty
 }
--- src/test/scala/com/atanana/parsers/TeamsPageParserTest.scala
+++ src/test/scala/com/atanana/parsers/TeamsPageParserTest.scala
@@ -9,20 +9,16 @@ import scala.io.Source
 //noinspection SourceNotClosed
 class TeamsPageParserTest extends AnyWordSpecLike with Matchers {
   val parser = new TeamsPageParser
-  private val html = Source.fromFile("src/test/scala/com/atanana/parsers/allTeams.html", "cp1251").getLines().mkString
-  private val teams = parser.getTeams(html)
+  private val content = Source.fromFile("src/test/scala/com/atanana/parsers/allTeams.json").getLines().mkString
+  private val teams = parser.getTeams(content)
 
   "TeamsPageParser" should {
     "parse valid teams count" in {
-      teams should have size 596
+      teams should have size 100
     }
 
-    "parse valid real team" in {
-      teams.head shouldEqual Team(45556, "Рабочее название", "Санкт-Петербург", 9921, 1.0f)
-    }
-
-    "parse valid virtual team" in {
-      teams(13) shouldEqual Team(64861, "Рислинг", "сборная", 7381, 10.5f, isReal = false)
+    "parse valid team" in {
+      teams.head shouldEqual Team(45556, "Рабочее название", "Санкт-Петербург", 12088, 1.0f)
     }
   }
 }
