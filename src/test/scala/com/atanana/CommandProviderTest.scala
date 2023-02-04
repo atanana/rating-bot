eda1483c2186171ec7bc4a7a25977f1123b20201
Andrei Tanana <tanana.andrey@gmail.com>
2023-02-04 20:08:50 +0300

    fix tests

--- src/test/scala/com/atanana/CommandProviderTest.scala
+++ src/test/scala/com/atanana/CommandProviderTest.scala
@@ -1,58 +1,132 @@
 package com.atanana
 
 import org.scalamock.scalatest.MockFactory
+import org.scalatest.BeforeAndAfter
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
 
+import java.net.{ServerSocket, Socket, SocketAddress, SocketOption}
 import java.nio.ByteBuffer
 import java.nio.channels.spi.SelectorProvider
 import java.nio.channels.{ServerSocketChannel, SocketChannel}
+import java.util
 import scala.util.Success
 
-class CommandProviderTest extends AnyWordSpecLike with MockFactory with Matchers {
-  private val socket = stub[TestSocket]
-  private val socketChannel = stub[TestSocketChannel]
+class CommandProviderTest extends AnyWordSpecLike with Matchers with BeforeAndAfter {
+  private val socket = new TestSocket(null)
+  private val socketChannel = new TestSocketChannel(null)
   private val provider = new CommandProvider(socket)
+
+  after {
+    socketChannel.reset()
+  }
 
   "CommandProvider" should {
     "provide no command when socket is null" in {
-      (socket.accept _).when().returns(null)
+      socket.socketChannel = null
       provider.getCommand shouldEqual Success(None)
     }
 
     "provide no command when socket is empty" in {
-      (socket.accept _).when().returns(socketChannel)
-      (socketChannel.read(_: ByteBuffer)).when(*).returns(0)
+      socket.socketChannel = socketChannel
+      socketChannel.bytes = Array.emptyByteArray
+      socketChannel.readResult = 0
       provider.getCommand shouldEqual Success(None)
     }
 
     "provide valid command" in {
-      (socket.accept _).when().returns(socketChannel)
+      socket.socketChannel = socketChannel
 
-      (socketChannel.read(_: ByteBuffer)).when(*).onCall((buffer: ByteBuffer) => {
-        buffer.put("test command\n".getBytes)
-        123
-      }).noMoreThanOnce()
+      socketChannel.bytes = "test command\n".getBytes
+      socketChannel.readResult = 123
+
       provider.getCommand shouldEqual Success(Some("test command"))
+      socketChannel.readCount shouldEqual 1
 
       //check that buffer was cleared
-      (socketChannel.read(_: ByteBuffer)).when(*).onCall((buffer: ByteBuffer) => {
-        buffer.put("tttt\n".getBytes)
-        123
-      }).noMoreThanOnce()
+      socketChannel.bytes = "tttt\n".getBytes
+      socketChannel.readResult = 123
+
       provider.getCommand shouldEqual Success(Some("tttt"))
+      socketChannel.readCount shouldEqual 2
     }
   }
 }
 
-abstract private class TestSocket(provider: SelectorProvider) extends ServerSocketChannel(provider) {
-  protected def implConfigureBlocking(block: Boolean): Unit
+class TestSocket(provider: SelectorProvider) extends ServerSocketChannel(provider) {
 
-  protected def implCloseSelectableChannel(): Unit
+  var socketChannel: SocketChannel = _
+
+  override def accept(): SocketChannel = socketChannel
+
+  override def bind(local: SocketAddress, backlog: Int): ServerSocketChannel = ???
+
+  override def setOption[T](name: SocketOption[T], value: T): ServerSocketChannel = ???
+
+  override def socket(): ServerSocket = ???
+
+  override def getLocalAddress: SocketAddress = ???
+
+  override def getOption[T](name: SocketOption[T]): T = ???
+
+  override def supportedOptions(): util.Set[SocketOption[_]] = ???
+
+  override def implCloseSelectableChannel(): Unit = ???
+
+  override def implConfigureBlocking(block: Boolean): Unit = ???
 }
 
-abstract private class TestSocketChannel(provider: SelectorProvider) extends SocketChannel(provider) {
-  protected def implConfigureBlocking(block: Boolean): Unit
+class TestSocketChannel(provider: SelectorProvider) extends SocketChannel(provider) {
 
-  protected def implCloseSelectableChannel(): Unit
+  var readResult: Int = _
+  var bytes: Array[Byte] = _
+  var readCount = 0
+
+  override def read(dst: ByteBuffer): Int = {
+    dst.put(bytes)
+    readCount += 1
+    readResult
+  }
+
+  def reset(): Unit = {
+    readCount = 0
+    readResult = 0
+    bytes = Array.emptyByteArray
+  }
+
+  override def bind(local: SocketAddress): SocketChannel = ???
+
+  override def setOption[T](name: SocketOption[T], value: T): SocketChannel = ???
+
+  override def shutdownInput(): SocketChannel = ???
+
+  override def shutdownOutput(): SocketChannel = ???
+
+  override def socket(): Socket = ???
+
+  override def isConnected: Boolean = ???
+
+  override def isConnectionPending: Boolean = ???
+
+  override def connect(remote: SocketAddress): Boolean = ???
+
+  override def finishConnect(): Boolean = ???
+
+  override def getRemoteAddress: SocketAddress = ???
+
+  override def read(dsts: Array[ByteBuffer], offset: Int, length: Int): Long = ???
+
+  override def write(src: ByteBuffer): Int = ???
+
+  override def write(srcs: Array[ByteBuffer], offset: Int, length: Int): Long = ???
+
+  override def getLocalAddress: SocketAddress = ???
+
+  override def getOption[T](name: SocketOption[T]): T = ???
+
+  override def supportedOptions(): util.Set[SocketOption[_]] = ???
+
+  override def implCloseSelectableChannel(): Unit = {}
+
+  override def implConfigureBlocking(block: Boolean): Unit = ???
 }
