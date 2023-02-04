aca0642eee6f9b79c53ecaa1845930440c7d405d
Andrei Tanana <tanana.andrey@gmail.com>
2023-02-04 23:11:14 +0300

    fix tests

--- src/test/scala/com/atanana/processors/ReminderProcessorTest.scala
+++ src/test/scala/com/atanana/processors/ReminderProcessorTest.scala
@@ -1,10 +1,11 @@
 package com.atanana.processors
 
 import cats.data.EitherT
-import com.atanana.MessageComposer
+import com.atanana.MessageComposerImpl
 import com.atanana.TestUtils.{getResult, getResultErrorMessage}
 import com.atanana.data.{Data, Requisition}
-import com.atanana.json.JsonStore
+import com.atanana.json.JsonStoreImpl
+import com.atanana.mocks.{MockJsonStore, MockMessageComposer, MockPoster}
 import com.atanana.posters.Poster
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.matchers.should.Matchers
@@ -13,39 +14,40 @@ import org.scalatest.wordspec.AnyWordSpecLike
 import java.time.LocalDateTime
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class ReminderProcessorTest extends AnyWordSpecLike with MockFactory with Matchers {
-  private val store = stub[JsonStore]
-  private val messageComposer = stub[MessageComposer]
-  private val poster = mock[Poster]
-  private val processor = new ReminderProcessor(store, messageComposer, poster)
+class ReminderProcessorTest extends AnyWordSpecLike with Matchers {
+  private val store = new MockJsonStore()
+  private val messageComposer = new MockMessageComposer()
+  private val poster = new MockPoster()
+  private val processor = new ReminderProcessorImpl(store, messageComposer, poster)
 
   "ReminderProcessor" should {
     "post correct reminders for tomorrow's requisitions" in {
       val requisition = Requisition("tournament 1", "agent 1", LocalDateTime.now().plusDays(1))
-      (() => store.read).when().returns(Data(Set.empty, Set(requisition)))
-      (messageComposer.composeRequisitionReminder _).when(requisition).returns("reminder")
-      (poster.postAsync _).expects("reminder") returns EitherT.rightT(())
+      store.data = Data(Set.empty, Set(requisition))
+      messageComposer.requisitionReminderMessages.put(requisition, "reminder")
+      poster.responses.put("reminder", EitherT.rightT(()))
 
       getResult(processor).isRight shouldBe true
+      poster.responses shouldBe empty
     }
 
     "not remind about not tomorrow's requisitions" in {
-      (() => store.read).when().returns(Data(Set.empty, Set(
+      store.data = Data(Set.empty, Set(
         Requisition("tournament 1", "agent 1", LocalDateTime.now()),
         Requisition("tournament 1", "agent 1", LocalDateTime.now().plusDays(2)),
         Requisition("tournament 1", "agent 1", LocalDateTime.now().plusMonths(1)),
         Requisition("tournament 1", "agent 1", LocalDateTime.now().minusDays(1)),
         Requisition("tournament 1", "agent 1", LocalDateTime.now().minusMonths(1))
-      )))
+      ))
 
       getResult(processor).isRight shouldBe true
     }
 
     "pass error from poster" in {
       val requisition = Requisition("tournament 1", "agent 1", LocalDateTime.now().plusDays(1))
-      (() => store.read).when().returns(Data(Set.empty, Set(requisition)))
-      (messageComposer.composeRequisitionReminder _).when(requisition).returns("reminder")
-      (poster.postAsync _).expects("reminder") returns EitherT.leftT(new RuntimeException("123"))
+      store.data = Data(Set.empty, Set(requisition))
+      messageComposer.requisitionReminderMessages.put(requisition, "reminder")
+      poster.responses.put("reminder", EitherT.leftT(new RuntimeException("123")))
 
       getResultErrorMessage(processor) shouldEqual "123"
     }
