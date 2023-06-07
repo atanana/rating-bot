2f49ad9e0b9c3d1b17e7e17a066ca3d7e8ae155b
Andrei Tanana <tanana.andrey@gmail.com>
2023-06-07 21:01:12 +0300

    migrate to using

--- src/main/scala/com/atanana/fs/FsHandlerImpl.scala
+++ src/main/scala/com/atanana/fs/FsHandlerImpl.scala
@@ -2,26 +2,17 @@ package com.atanana.fs
 
 import java.io.PrintWriter
 import scala.io.Source
-import scala.util.Try
+import scala.util.{Try, Using}
 
 class FsHandlerImpl extends FsHandler {
-  override def readFile(filename: String): Try[String] = {
-    Try {
-      val source = Source.fromFile(filename)
-      try {
-        source.getLines().mkString
-      } finally {
-        source.close()
-      }
+  override def readFile(filename: String): Try[String] =
+    Using(Source.fromFile(filename)) { source =>
+      source.getLines().mkString
     }
-  }
 
   override def writeFile(contents: String, filename: String): Unit = {
-    val writer = new PrintWriter(filename)
-    try {
+    Using(new PrintWriter(filename)) { writer =>
       writer.print(contents)
-    } finally {
-      writer.close()
     }
   }
 }
