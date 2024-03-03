4f55909c28664c0d27ca639c472c7de04de60627
Andrei Tanana <tanana.andrey@gmail.com>
2024-03-03 22:36:25 +0300

    add socket error logging

--- src/main/scala/com/atanana/ratingbot/CommandProvider.scala
+++ src/main/scala/com/atanana/ratingbot/CommandProvider.scala
@@ -1,16 +1,25 @@
 package com.atanana.ratingbot
 
+import com.typesafe.scalalogging.Logger
+
 import java.io.{BufferedReader, InputStreamReader}
 import java.net.ServerSocket
-import scala.util.Try
+import scala.util.control.NonFatal
+import scala.util.{Failure, Success, Try}
 
 class CommandProvider(serverSocket: ServerSocket) {
 
-  def getCommand: Try[String] = {
-    Try {
+  private val logger = Logger("CommandProvider")
+
+  def getCommand: Try[String] =
+    try {
       val socket = serverSocket.accept()
       val reader = new BufferedReader(new InputStreamReader(socket.getInputStream))
-      reader.readLine()
+      Success(reader.readLine())
     }
-  }
+    catch {
+      case NonFatal(e) =>
+        logger.error("Error in socket!", e)
+        Failure(e)
+    }
 }
